034e4514930846636e79d45adeaf98db42ed764c
Julio Sánchez <66069314+juliosanchez7@users.noreply.github.com>
2021-06-20 23:03:53 -0400

    SplashBar

--- xpenses_app/lib/screens/splash.dart
+++ xpenses_app/lib/screens/splash.dart
@@ -1,12 +1,15 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:xpenses_app/screens/tapBar/tapBar.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: 5), () {
-      print('Holi');
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => MyTabBar(),
+      ));
     });
     return Container(
       color: Colors.white,
