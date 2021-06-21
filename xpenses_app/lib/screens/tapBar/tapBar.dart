ce62d4ae7fa3442ad0c2fd66c6fe683a7caafc2c
alejandro <alejandro.morales.a@gmail.com>
2021-06-20 22:55:46 -0400

    tabbar

---
+++ xpenses_app/lib/screens/tapBar/tapBar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:xpenses_app/screens/tapBar/tapbarController.dart';
+
+class MyTabBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final MyTabController _tabx = Get.put(MyTabController());
+    // ↑ init tab controller
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Xpenses"),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          color: Colors.grey[500],
+          child: TabBar(
+            controller: _tabx.controller,
+            tabs: _tabx.myTabs,
+          ),
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabx.controller,
+        children: _tabx.myTabs.map((Tab tab) {
+          final String label = tab.text!.toLowerCase();
+          switch (label) {
+            case 'incomes':
+              return Container(
+                child: Text('income tab'),
+              );
+            case 'expenses':
+              return Container(
+                child: Text("expenses tab"),
+              );
+            default:
+              return Center(
+                child: Text(
+                  'This is the $label tab',
+                  style: const TextStyle(fontSize: 36),
+                ),
+              );
+          }
+        }).toList(),
+      ),
+    );
+  }
+}
